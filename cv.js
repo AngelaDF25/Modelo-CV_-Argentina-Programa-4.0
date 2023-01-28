363561bcbbfc7e82e0975efb38597c0999908763
Angela Di Fiore <angeladifiore13@gmail.com>
2023-01-27 22:47:02 -0300

    proyecto cv argentina programa

---
+++ cv.js
@@ -0,0 +1,24 @@
+/*
+
+async function consultar () {
+    let= await fetch ("https://randomuser.me/api/?results=1&gender=female")
+                        .then ( Response => Response.json () )
+                        .then (data => console.log(data));
+    
+}
+
+consultar ()
+
+alert("Mensaje externo");
+
+*/
+const nombre = document.querySelector ('#nombre');
+
+async function consultar () {
+    let= await fetch ("https://randomuser.me/api/?results=1&gender=female")
+                        .then ( Response => Response.json () )
+                        .then (data => console.log(data));
+    
+}
+
+consultar ()
